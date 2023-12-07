017114fd85fab0ce07d20f350ed185d2622249ce
Duy Phan <phanquangduy01@gmail.com>
2023-12-07 20:06:53 +0700

    update

--- src/test/java/selenium4/com/common/BaseTest.java
+++ src/test/java/selenium4/com/common/BaseTest.java
@@ -1,7 +1,10 @@
 package selenium4.com.common;
 
+import static selenium4.com.constants.FrameworkConstants.BROWSER;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ThreadGuard;
+import org.testng.Reporter;
 import org.testng.annotations.*;
 
 import selenium4.com.driver.DriverManager;
@@ -11,10 +14,16 @@ import selenium4.com.listeners.TestListener;
 
 //@Listeners({TestListener.class})
 public class BaseTest {
-	@Parameters("BROWSER")
+	//@Parameters("BROWSER")
 	@BeforeMethod(alwaysRun = true)
-	public void createDriver(@Optional("chrome") String browser) {
+	public void createDriver() {
+		String browser;
 		System.setProperty("webdriver.http.factory", "jdk-http-client");
+		if (System.getProperty("BROWSER") != null) {
+			browser = System.getProperty("BROWSER").toLowerCase();
+		} else {
+			browser = BROWSER.toLowerCase();
+		}
 		System.out.println("----- Browser: " + browser);
 		WebDriver driver = ThreadGuard.protect(new TargetFactory().createInstance(browser));
 		DriverManager.setDriver(driver);
